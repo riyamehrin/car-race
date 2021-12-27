31a7280164ea0528e02ba781521a6b9928074555
riyamehrin <86054695+riyamehrin@users.noreply.github.com>
2021-12-27 19:33:50 +0530

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,13 @@
 class Game {
-  constructor() {}
+  constructor() {
+    this.resetTitle = createElement("h2");
+    this.resetButton = createButton("");
+
+    this.leadeboardTitle = createElement("h2");
+
+    this.leader1 = createElement("h2");
+    this.leader2 = createElement("h2");
+  }
 
   getState() {
     var gameStateRef = database.ref("gameState");
@@ -35,15 +43,36 @@ class Game {
     form.hide();
     form.titleImg.position(40, 50);
     form.titleImg.class("gameTitleAfterEffect");
+
+    //C39
+    this.resetTitle.html("Reset Game");
+    this.resetTitle.class("resetText");
+    this.resetTitle.position(width / 2 + 200, 40);
+
+    this.resetButton.class("resetButton");
+    this.resetButton.position(width / 2 + 230, 100);
+
+    this.leadeboardTitle.html("Leaderboard");
+    this.leadeboardTitle.class("resetText");
+    this.leadeboardTitle.position(width / 3 - 60, 40);
+
+    this.leader1.class("leadersText");
+    this.leader1.position(width / 3 - 50, 80);
+
+    this.leader2.class("leadersText");
+    this.leader2.position(width / 3 - 50, 130);
   }
 
   play() {
     this.handleElements();
+    this.handleResetButton();
 
     Player.getPlayersInfo();
 
     if (allPlayers !== undefined) {
       image(track, 0, -height * 5, width, height * 6);
+
+      this.showLeaderboard();
 
       //index of the array
       var index = 0;
@@ -57,25 +86,91 @@ class Game {
 
         cars[index - 1].position.x = x;
         cars[index - 1].position.y = y;
-        if (index==player.index){
-          stroke (10);
-          fill ("blue");
-          ellipse(x,y,60,100);
-          camera.position.x=cars[index-1].position.x;
-          camera.position.y=cars[index-1].position.y;
+
+        if (index === player.index) {
+          stroke(10);
+          fill("red");
+          ellipse(x, y, 60, 60);
+
+          // Changing camera position in y direction
+          camera.position.y = cars[index - 1].position.y;
         }
       }
 
+      // handling keyboard events
       this.handlePlayerControls();
-
+      const finishLine =height*6-100;
+      if (player.positionY>finishLine){
+        player.rank+=1;
+        player.update()
+      }
       drawSprites();
     }
   }
 
+  handleResetButton() {
+    this.resetButton.mousePressed(()=>{
+      database.ref('/').set({
+        playerCount:0,gameState:0,players:{}
+      })
+      window.location.reload()
+    })
+  }
+
+  showLeaderboard() {
+    var leader1, leader2;
+    var players = Object.values(allPlayers);
+    if (
+      (players[0].rank === 0 && players[1].rank === 0) ||
+      players[0].rank === 1
+    ) {
+      // &emsp;    This tag is used for displaying four spaces.
+      leader1 =
+        players[0].rank +
+        "&emsp;" +
+        players[0].name +
+        "&emsp;" +
+        players[0].score;
+
+      leader2 =
+        players[1].rank +
+        "&emsp;" +
+        players[1].name +
+        "&emsp;" +
+        players[1].score;
+    }
+
+    if (players[1].rank === 1) {
+      leader1 =
+        players[1].rank +
+        "&emsp;" +
+        players[1].name +
+        "&emsp;" +
+        players[1].score;
+
+      leader2 =
+        players[0].rank +
+        "&emsp;" +
+        players[0].name +
+        "&emsp;" +
+        players[0].score;
+    }
+
+    this.leader1.html(leader1);
+    this.leader2.html(leader2);
+  }
+
   handlePlayerControls() {
-    // handling keyboard events
     if (keyIsDown(UP_ARROW)) {
       player.positionY += 10;
+      player.update();
+    }
+    if (keyIsDown(LEFT_ARROW)  && player.positionX>width/3-50) {
+      player.positionX -= 5;
+      player.update();
+    }
+    if (keyIsDown(RIGHT_ARROW)  && player.positionX>width/2+300) {
+      player.positionX += 5;
       player.update();
     }
   }
